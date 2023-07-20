c10e5f957a1044b73789d87ec03d9d42437c7d4e
SamBridgess <81350195+SamBridgess@users.noreply.github.com>
2023-07-20 22:48:26 +0300

    Async

--- main.go
+++ main.go
@@ -17,9 +17,11 @@ type Config struct {
 
 var configuration = Config{}
 var configFilePath = "config.json"
+var files []os.DirEntry
 
 func main() {
 	readConfig()
+	files, _ = os.ReadDir(configuration.PicsFolder)
 	bot, _ := tgbotapi.NewBotAPI(configuration.TelegramBotToken)
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -27,30 +29,41 @@ func main() {
 	u.Timeout = 60
 
 	updates, _ := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	msgCh := make(chan tgbotapi.Update)
 
-		if update.Message.Text == "/get_furry" {
-			filePath := configuration.PicsFolder + getRandomImage()
-			msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, filePath)
-			bot.Send(msg)
-			log.Printf("Response %s", filePath)
+	go func() {
+		for update := range updates {
+			msgCh <- update
 		}
+	}()
+
+	for update := range msgCh {
+		go handleMessage(update, bot)
+	}
+}
+
+func handleMessage(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+	if update.Message == nil {
+		return
+	}
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+	if update.Message.Text == "/get_furry" {
+		filePath := configuration.PicsFolder + getRandomImage()
+		msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, filePath)
+		bot.Send(msg)
+		log.Printf("Response %s", filePath)
 	}
 }
 func getRandomImage() string {
-	files, _ := os.ReadDir(configuration.PicsFolder)
 	rand.Seed(time.Now().UTC().UnixNano())
 	return files[rand.Intn(len(files))].Name()
 }
 func readConfig() {
-	f, _ := os.Open(configFilePath)
+	f, err := os.Open(configFilePath)
 
 	decoder := json.NewDecoder(f)
-	err := decoder.Decode(&configuration)
+	err = decoder.Decode(&configuration)
 	if err != nil {
 		fmt.Println("An error occurred while reading config")
 	}
